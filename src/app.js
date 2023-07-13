530a21e5a0c9e16fc0e4aec37249719179e344d8
juliestasz <juliestaszczak@gmail.com>
2023-07-13 12:03:03 +0200

    added JS functions - 1st stage

---
+++ src/app.js
@@ -0,0 +1,12 @@
+function displayTemperature(response) {
+  console.log(response.data);
+  let temperatureElement = document.querySelector("#temperature");
+  let cityElement = document.querySelector("#city");
+  temperatureElement.innerHTML = Math.round(response.data.main.temp);
+  cityElement.innerHTML = response.data.name;
+}
+
+let apiKey = "36811a910edc7556e3f396c180198841";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=New York&appid=${apiKey}&units=metric`;
+
+axios.get(apiUrl).then(displayTemperature);
